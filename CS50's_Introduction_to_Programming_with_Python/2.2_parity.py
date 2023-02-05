993f517da3e4acfbc695e96328af2ab86a859ce9
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-02-05 19:24:36 -0300

    Create 2.2_parity.py

---
+++ CS50's_Introduction_to_Programming_with_Python/2.2_parity.py
@@ -0,0 +1,54 @@
+#Ex1.
+#x = int(input("What's x?"))
+
+#if x % 2 == 0:
+#  print("Even")
+#else:
+#  print("Odd")
+
+  
+#Ex2.
+#def main():
+#  x = int(input("What's x?"))
+#  if is_even(x):
+#    print("Even")
+#  else:
+#  print("Odd")
+
+#def is_even(n):
+#  if n % 2 == 0:
+#    return True
+#    else:
+#      return False
+#main()
+
+
+
+  
+#Ex3. Pythonico + elegante
+#def main():
+#  x = int(input("What's x?"))
+#  if is_even(x):
+#    print("Even")
+#  else:
+#  print("Odd")
+
+#def is_even(n):
+#  return True if n % 2 == 0 else False:
+
+#main()
+
+
+#Ex.3.1
+def main():
+  x = int(input("What's x?"))
+  if is_even(x):
+    print("Even")
+  else:
+  print("Odd")
+
+def is_even(n):
+  return n % 2 == 0
+
+main()
+
