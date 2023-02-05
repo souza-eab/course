4d304332f09367376e763766d2a0cfb101529c45
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-02-05 16:44:57 -0300

    Create 1.1_calculator.py

---
+++ CS50's_Introduction_to_Programming_with_Python/1.1_calculator.py
@@ -0,0 +1,37 @@
+'''
+Calculator
+'''
+
+#Ex1
+#x= 1
+#y = 2
+#z = x+y
+
+#print(z)
+
+# Ex2
+
+#x = input("What's x? ")
+#y = input("What's y? ")
+#z = x+y #bug pq concatena os numeros e não soma
+
+
+
+# Ex3 (Precisamos da variavel z?
+
+#x = input("What's x? ")
+#y = input("What's y? ")
+#z = int(x)+int(y) # Aí funciona pq declaramos como inteiro
+
+
+
+# Ex4 (Daria para fazer em uma linha
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+print (x+y) # Aí funciona pq declaramos como inteiro
+
+
+
+# Ex5 Qual o problema em simplificar demais ? Antes o usuário tem que lidar com x e y | ex de simplificar demais nem sempre é uma boa; 
+print(int(input("What's x? "))+ int(input("What's y? "))
