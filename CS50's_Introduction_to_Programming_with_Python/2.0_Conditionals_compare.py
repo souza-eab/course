340840a5538f9f5364ec37a241fc6944af9030d9
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-02-05 18:36:34 -0300

    Create 2.0_Conditionals_compare.py

---
+++ CS50's_Introduction_to_Programming_with_Python/2.0_Conditionals_compare.py
@@ -0,0 +1,39 @@
+#Ex1 - utilizando if and elif
+
+#x = int(input("What's x?"))
+#y = int(input("What's y?"))
+
+#if x <y:
+#  print("x is less than y")
+#  elif x>y:
+#    print("x is greater than y")
+#    elif x==y:
+#      print("x is equal to y")
+      
+#Ex2 - utilizando else e reduz a ultima linha
+#x = int(input("What's x?"))
+#y = int(input("What's y?"))
+#if x <y:
+#  print("x is less than y")
+#  elif x>y:
+#    print("x is greater than y")
+#    else:
+#      print("x is equal to y")
+
+
+#Ex3 - utilizando or
+#x = int(input("What's x?"))
+#y = int(input("What's y?"))
+#if x <y or x> y:
+#  print("x is lnot equal to y")
+#  else:
+#     print("x is equal to y")
+      
+
+#Ex4 - utilizando or
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+if x != y: #==
+  print("x is lnot equal to y")
+  else:
+     print("x is equal to y")
